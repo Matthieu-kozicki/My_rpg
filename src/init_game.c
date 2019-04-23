a5773b7ca5868b7c1efd128c68e684fc8b378a2b
Kozicki Matthieu <matthieu.kozicki@epitech.eu>
2019-04-23 19:33:37 +0200

    [ADDED] quest sprite

--- src/init_game.c
+++ src/init_game.c
@@ -30,6 +30,7 @@ void init_game_variables2(game_t *game, object_t *obj)
 {
     game->volume = 100;
     sfSprite_setScale(obj[35].spr,(sfVector2f){0.45, 0.45});
+    sfSprite_setScale(obj[36].spr,(sfVector2f){0.07, 0.07});
 }
 
 void init_game_variables(game_t *game, object_t *obj)
--- src/play_game.c
+++ src/play_game.c
@@ -19,6 +19,7 @@ void play_game(game_t *game, object_t *obj)
     sfRenderWindow_setView (game->window, game->view);
     sfRenderWindow_drawSprite(game->window, obj[4].spr, NULL);
     sfRenderWindow_drawSprite(game->window, obj[33].spr, NULL);
+    sfRenderWindow_drawSprite(game->window, obj[36].spr, NULL);
     sfRenderWindow_drawSprite(game->window, obj[31].spr, NULL);
     sfSprite_setTextureRect(obj[4].spr, obj[4].rect);
     if (sfKeyboard_isKeyPressed(sfKeyI))
