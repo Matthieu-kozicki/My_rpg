f5933b20f127d561ea7a45b722c55401801fc69a
Kozicki Matthieu <matthieu.kozicki@epitech.eu>
2019-05-04 12:16:36 +0200

    [FIXED] coding style

--- src/play_game.c
+++ src/play_game.c
@@ -7,6 +7,49 @@
 
 #include "my_rpg.h"
 
+
+void quest_next(game_t *game, object_t *obj)
+{
+    if (game->pos_x == 28 && game->pos_y == 87
+        && sfKeyboard_isKeyPressed(sfKeyE) && obj->quest >= 3) {
+        game->screen = 6;
+        sfSleep((sfTime) {200000});
+    }
+
+    if (obj->quest == 7) {
+        find_randpoke(game);
+        game->combat->difficulty = 6;
+        game->combat->poke[0].info[NAME] = "A S T E K";
+        game->combat->poke[0].info[ATK_NAME] = "Man Google !";
+        game->combat->poke[0].stats[HP] = randint(90, 200);
+        game->combat->poke[0].stats[DEF] = randint(5, 20);
+        game->combat->poke[0].stats[ATK] = randint(10, 35);
+        load_poke_sprites(game->combat);
+        game->screen = 5;
+        obj->quest = 8;
+    }
+}
+
+void play_quest(game_t *game, object_t *obj)
+{
+    if (game->pos_x == 27 && game->pos_y == 94
+        && sfKeyboard_isKeyPressed(sfKeyE) && obj->quest == 1) {
+        obj->quest = 2;
+        sfSleep((sfTime) {200000});
+    }
+    if (game->pos_x == 19 && game->pos_y == 47
+        && sfKeyboard_isKeyPressed(sfKeyE) && obj->quest == 3) {
+        obj->quest = 4;
+        sfSleep((sfTime) {200000});
+    }
+    if (game->pos_x == 36 && game->pos_y == 12
+        && sfKeyboard_isKeyPressed(sfKeyE) && obj->quest == 5) {
+        obj->quest = 6;
+        sfSleep((sfTime) {200000});
+    }
+    quest_next(game, obj);
+
+}
 void play_game(game_t *game, object_t *obj)
 {
     game->time = sfClock_getElapsedTime(game->clock);
@@ -26,37 +69,6 @@ void play_game(game_t *game, object_t *obj)
         game->screen = 7;
     if (sfKeyboard_isKeyPressed(sfKeyP))
         quest(game, obj);
-    if (game->pos_x == 27 && game->pos_y == 94
-        && sfKeyboard_isKeyPressed(sfKeyE) && obj->quest == 1) {
-        obj->quest = 2;
-        sfSleep((sfTime) {200000});
-    }
-    if (game->pos_x == 19 && game->pos_y == 47
-        && sfKeyboard_isKeyPressed(sfKeyE) && obj->quest == 3) {
-        obj->quest = 4;
-        sfSleep((sfTime) {200000});
-    }
-    if (game->pos_x == 36 && game->pos_y == 12
-        && sfKeyboard_isKeyPressed(sfKeyE) && obj->quest == 5) {
-        obj->quest = 6;
-        sfSleep((sfTime) {200000});
-    }
-    if (game->pos_x == 28 && game->pos_y == 87
-        && sfKeyboard_isKeyPressed(sfKeyE) && obj->quest >= 3) {
-        game->screen = 6;
-        sfSleep((sfTime) {200000});
-    }
-    if (obj->quest == 7) {
-        find_randpoke(game);
-        game->combat->difficulty = 6;
-        game->combat->poke[0].info[NAME] = "A S T E K";
-        game->combat->poke[0].info[ATK_NAME] = "Man Google !";        
-        game->combat->poke[0].stats[HP] = randint(90, 200);
-        game->combat->poke[0].stats[DEF] = randint(5, 20);
-        game->combat->poke[0].stats[ATK] = randint(10, 35);
-        load_poke_sprites(game->combat);
-        game->screen = 5;
-        obj->quest = 8;
-    }
+    play_quest(game, obj);
     dialogue(game, obj);
 }
